b0d46e556c67502b917a7d2f30efac468428c797
Teos <47872542+teofanis@users.noreply.github.com>
2022-03-17 20:06:53 +0200

    `mix()` assets (#4)

--- src/SearchableDropdownServiceProvider.php
+++ src/SearchableDropdownServiceProvider.php
@@ -21,20 +21,20 @@ class SearchableDropdownServiceProvider extends ServiceProvider
       Blade::component('searchableDropdown::partials.list', 'searchable-dropdown-list');
       Blade::component('searchableDropdown::partials.item', 'searchable-dropdown-list-item');
 
-      
+
       /** Register Directives */
       Blade::directive('searchableDropdownStyles', function(){
-        return '<link rel="stylesheet" href="{{asset(\'/css/searchable-dropdown-styles.css\')}}">';
+        return '<link rel="stylesheet" href="{{mix(\'/css/searchable-dropdown-styles.css\')}}">';
       });
 
       Blade::directive('searchableDropdownScripts', function(){
-        return '<script src="{{asset(\'/js/searchable-dropdown-scripts.js\')}}"></script>';
+        return '<script src="{{mix(\'/js/searchable-dropdown-scripts.js\')}}"></script>';
       });
 
       Blade::directive('bindJSInstance', function(){
         return '<script> (function(){ var event = new CustomEvent(\'search-dropdown-ready\', { detail:"{{$dropdownFunctionName}}" }); window.dispatchEvent(event);})(); </script>';
       });
-      
+
       $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'searchable-dropdown-config.php');
       $this->mergeConfigFrom(__DIR__.'/../config/props.php', 'searchable-dropdown-props.php');
 
@@ -50,10 +50,10 @@ class SearchableDropdownServiceProvider extends ServiceProvider
       //Load helpers
       if(File::exists(__DIR__.'/helpers.php')){
         require __DIR__.'/helpers.php';
-      }  
+      }
       /** Config Publishing */
-      $this->publishes([__DIR__.'/../config/config.php' => config_path('searchable-dropdown-config.php'), __DIR__.'/../config/props.php' => config_path('searchable-dropdown-props.php')], 'searchable-dropdown-config');
+    //   $this->publishes([__DIR__.'/../config/config.php' => config_path('searchable-dropdown-config.php'), __DIR__.'/../config/props.php' => config_path('searchable-dropdown-props.php')], 'searchable-dropdown-config');
       /** Asset Publishing */
-      $this->publishes([__DIR__.'/assets/js' => public_path('js'),  __DIR__.'/assets/css' => public_path('css')],'searchable-dropdown-assets');
+    //   $this->publishes([__DIR__.'/assets/js' => public_path('js'),  __DIR__.'/assets/css' => public_path('css')],'searchable-dropdown-assets');
     }
 }
